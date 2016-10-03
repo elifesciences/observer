97e6430495edb012a1054b9b390fe1f37c136117
Luke Skibinski <l.skibinski@elifesciences.org>
2016-10-03 13:05:28 +0100

    added test & lint scripts

---
+++ .test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e # everything must pass
+
+args="$@"
+module="src"
+print_coverage=1
+if [ ! -z "$args" ]; then
+    module="$args"
+    print_coverage=0
+fi
+
+# remove any old compiled python files
+find src/ -name '*.pyc' -delete
+
+# called by test.sh
+rm -f build/junit.xml
+#./src/manage.py test --testrunner=green.djangorunner.DjangoRunner "$@"
+coverage run --source='src/' --omit='*/tests/*,*/migrations/*' src/manage.py test "$module" --no-input
+echo "* passed tests"
+
+# run coverage test
+# only report coverage if we're running a complete set of tests
+if [ $print_coverage -eq 1 ]; then
+    coverage report
+    # is only run if tests pass
+    covered=$(coverage report | grep TOTAL | awk '{print $4}' | sed 's/%//')
+    if [ $covered -lt 78 ]; then
+        echo
+        echo "FAILED this project requires at least 78% coverage, got $covered"
+        echo
+        exit 1
+    fi
+fi
+
