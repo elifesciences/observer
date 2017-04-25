7de5394fa4f5d46a72e02fa10f875800325fdae8
Luke Skibinski <l.skibinski@elifesciences.org>
2017-04-25 17:53:06 +0930

    fixed up another instance of previous bug, removed caused of bug

--- src/observer/tests/base.py
+++ src/observer/tests/base.py
@@ -1,17 +1,9 @@
 import os
-from os.path import join
 from django.test import TestCase
-from observer.utils import lmap, lfilter
 
 class BaseCase(TestCase):
     this_dir = os.path.dirname(os.path.realpath(__file__))
     fixture_dir = os.path.join(this_dir, 'fixtures')
-
-    def ajson_list(self):
-        path = join(self.fixture_dir, 'ajson')
-        is_ajson = lambda p: p.endswith('.xml.json')
-        fullpath = lambda fname: join(path, fname)
-        return lfilter(is_ajson, lmap(fullpath, os.listdir(path)))
 
     def freshen(self, obj):
         return type(obj).objects.get(pk=obj.pk)
