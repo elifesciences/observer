e85916851807e0ecaa24ab4a3a06de0bad3dcb4a
Luke Skibinski <l.skibinski@elifesciences.org>
2017-05-01 13:57:05 +0930

    added test to ensure default and explicit ordering of report results happens

--- src/observer/tests/test_rss_views.py
+++ src/observer/tests/test_rss_views.py
@@ -1,3 +1,5 @@
+from functools import reduce
+from datetime import datetime
 from observer.utils import listfiles
 import re
 from os.path import join
@@ -5,6 +7,7 @@ from .base import BaseCase
 from django.test import Client
 from django.core.urlresolvers import reverse
 from observer import ingest_logic, models
+from observer.utils import lmap
 
 '''
 from unittest import skip
@@ -68,14 +71,14 @@ class One(BaseCase):
 class Two(BaseCase):
     def setUp(self):
         self.c = Client()
+        for path in listfiles(join(self.fixture_dir, 'ajson'), ['.json']):
+            ingest_logic.file_upsert(path)
+        ingest_logic.regenerate_all()
 
     def tearDown(self):
         pass
 
     def test_report_paginated(self):
-        for path in listfiles(join(self.fixture_dir, 'ajson'), ['.json']):
-            ingest_logic.file_upsert(path)
-        ingest_logic.regenerate_all()
         expected_articles = 4 # 4 articles, 11 article versions
         self.assertEqual(models.Article.objects.count(), expected_articles)
 
@@ -88,3 +91,35 @@ class Two(BaseCase):
         regex = r"<item>"
         matches = re.findall(regex, xml)
         self.assertEqual(len(matches), expected_articles)
+
+    def test_report_ordered(self):
+        "ensure report is ordered correctly"
+        url = reverse('report', kwargs={'name': 'latest-articles'})
+        resp = self.c.get(url)
+        xml = resp.content.decode('utf-8')
+
+        regex = r"<dc:date>(.+)</dc:date>"
+        date_list = lmap(lambda dt: datetime.strptime(dt[:10], "%Y-%m-%d"), re.findall(regex, xml))
+
+        # given two dates, returns the smallest
+        # the result of reducing a list of dates should be the last element in the list
+        def rdf(d1, d2):
+            if not d2: # guard
+                return d1
+            return d2 if d1 >= d2 else d1
+        self.assertEqual(reduce(rdf, date_list), date_list[-1])
+
+    def test_report_ordered_reverse(self):
+        "ensure report is ordered correctly"
+        url = reverse('report', kwargs={'name': 'latest-articles'})
+        resp = self.c.get(url, {'order': 'ASC'})
+        xml = resp.content.decode('utf-8')
+
+        regex = r"<dc:date>(.+)</dc:date>"
+        date_list = lmap(lambda dt: datetime.strptime(dt[:10], "%Y-%m-%d"), re.findall(regex, xml))
+
+        # given two dates, returns the largest
+        # the result of reducing a list of dates should be the last element in the list
+        def rdf(d1, d2):
+            return d1 if d1 >= d2 else d2
+        self.assertEqual(reduce(rdf, date_list), date_list[-1])
