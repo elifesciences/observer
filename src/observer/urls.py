b6ed259d860129fd22dffb76b37276f052f23f4f
Luke Skibinski <l.skibinski@elifesciences.org>
2017-05-10 13:57:50 +0930

    removed trailing slash from reportname in urls

--- src/observer/urls.py
+++ src/observer/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^report/(?P<name>[\-\w]+)/$', views.report, name='report'),
+    url('^report/(?P<name>[\-\w]+)$', views.report, name='report'),
     url(r'^$', views.landing, name='pub-landing'),
 ]
