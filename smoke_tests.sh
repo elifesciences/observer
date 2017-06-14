3f3d796872ab02135ab7e598b2d804d321ad77a5
Giorgio Sironi <info@giorgiosironi.com>
2017-06-14 10:06:01 +0100

    Re-added reports with trailing slashes

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-for path in /; do #/report/latest-articles /report/upcoming-articles; do
+for path in / /report/latest-articles/ /report/upcoming-articles/; do
     [ $(curl --write-out %{http_code} --silent --output /dev/null https://$(hostname)$path) == 200 ]
 done
 
