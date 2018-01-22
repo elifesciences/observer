c916bbacea7a8221b277c0403b8e471454eb944f
Luke Skibinski <l.skibinski@elifesciences.org>
2018-01-22 16:13:35 +1030

    load_from_api cmd now supports loading a list of msids instead of 'all'

--- src/observer/management/commands/load_from_api.py
+++ src/observer/management/commands/load_from_api.py
@@ -1,14 +1,34 @@
 import sys
 from django.core.management.base import BaseCommand
-from observer.ingest_logic import download_all_article_versions, regenerate_all
+from observer.ingest_logic import (
+    download_all_article_versions, download_article_versions,
+    download_all_article_metrics, download_article_metrics,
+    regenerate_all, regenerate
+)
+from observer.utils import lmap
+from functools import partial
 
 class Command(BaseCommand):
     help = "a terrifically sequential and SLOW way to load ALL elife articles and versions"
 
+    def add_arguments(self, parser):
+        parser.add_argument('--msid', nargs='+', type=int, required=False)
+
     def handle(self, *args, **options):
         try:
-            download_all_article_versions()
-            regenerate_all()
+            dl_ajson = download_all_article_versions
+            dl_metrics = download_all_article_metrics
+            regen = regenerate_all
+
+            msidlist = options['msid']
+            if msidlist:
+                dl_ajson = partial(lmap, download_article_versions, msidlist)
+                dl_metrics = partial(lmap, download_article_metrics, msidlist)
+                regen = partial(lmap, regenerate, msidlist)
+
+            dl_ajson()
+            dl_metrics()
+            regen()
         except KeyboardInterrupt:
             print("\nctrl-c caught, quitting.\ndownload progress has been saved")
             sys.exit(1)
