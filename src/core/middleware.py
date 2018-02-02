6a5d51933d1ccba8c9899affc06be2a262b5ae88
Luke Skibinski <l.skibinski@elifesciences.org>
2018-02-02 12:03:32 +1030

    added tests for expected caching header values

--- src/core/middleware.py
+++ src/core/middleware.py
@@ -1,6 +1,9 @@
 from django.views.decorators.cache import patch_cache_control
 from django.utils.cache import patch_vary_headers
 
+MAX_AGE = 60 * 5
+MAX_STALE =  (60 * 60) * 24
+
 class DownstreamCaching(object):
     def __init__(self, get_response):
         self.get_response = get_response
@@ -11,9 +14,9 @@ class DownstreamCaching(object):
 
             # could probably bump these values to 10minutes.
 
-            'max-age': 60 * 5, # 5 minutes, 300 seconds
-            'stale-while-revalidate': 60 * 5, # 5 minutes, 300 seconds
-            'stale-if-error': (60 * 60) * 24, # 1 day, 86400 seconds
+            'max-age': MAX_AGE, # 5 minutes, 300 seconds
+            'stale-while-revalidate': MAX_AGE, # 5 minutes, 300 seconds
+            'stale-if-error': MAX_STALE, # 1 day, 86400 seconds
         }
         response = self.get_response(request)
 
--- src/core/tests.py
+++ src/core/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from core.middleware import MAX_AGE, MAX_STALE
 
 class DownstreamCaching(TestCase):
     def setUp(self):
@@ -17,6 +18,16 @@ class DownstreamCaching(TestCase):
         resp = self.c.get(self.url)
         for header in expected_headers:
             self.assertTrue(resp.has_header(header), "header %r not found in response" % header)
+
+    def test_cache_header_values(self):
+        expected_headers = [
+            ('vary', ['Accept']),
+            ('cache-control', ['max-age=%s' % MAX_AGE, 'public', 'stale-if-error=%s' % MAX_STALE, 'stale-while-revalidate=%s' % MAX_AGE])
+        ]
+        resp = self.c.get(self.url)
+        for header, expected in expected_headers:
+            # dirty parsing to guarantee ordered values
+            self.assertEqual(sorted(resp[header].split(', ')), expected)
 
     def test_cache_headers_not_in_response(self):
         cases = [
