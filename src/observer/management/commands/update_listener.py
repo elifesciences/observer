a146e88cb4de8527c27d7c7d819d907a86e38805
Luke Skibinski <l.skibinski@elifesciences.org>
2018-02-12 13:05:00 +1030

    update listener can now handle more than article updates

--- src/observer/management/commands/update_listener.py
+++ src/observer/management/commands/update_listener.py
@@ -2,50 +2,45 @@ import sys, json
 from django.conf import settings
 from django.core.management.base import BaseCommand
 import logging
-from observer import inc, ingest_logic
-from observer.utils import lmap
-import requests
+from observer import inc
+from observer.ingest_logic import download_regenerate_article, download_regenerate_presspackage
 
 LOG = logging.getLogger(__name__)
 
 class Command(BaseCommand):
-    help = 'open a connection to AWS SQS and listen for article update notifications'
+    help = 'open a connection to AWS SQS and listen for update notifications'
 
     def handle(self, *args, **options):
-        try:
-            if not settings.ARTICLE_EVENT_QUEUE:
-                raise ValueError("no queue name found. a queue name can be set in your 'app.cfg'. see example file 'elife.cfg'")
-
-            def action(event):
-                try:
-                    LOG.debug("handling event %s" % event)
-                    msid = json.loads(event)['id']
-                    ingest_logic.download_article_versions(msid)
-                    ingest_logic.regenerate(msid)
-
-                except requests.exceptions.RequestException as err:
-                    log = LOG.warn if err.response.status_code == 404 else LOG.error
-                    log("failed to fetch article %s: %s", msid, err)
-
-                except BaseException as err:
-                    LOG.exception("unhandled exception attempting to download and regenerate article %s", msid)
-
-            LOG.info("attempting connection %s ...", settings.ARTICLE_EVENT_QUEUE)
-
-            try:
-                import newrelic.agent
-                action = newrelic.agent.background_task()(action)
-            except ImportError:
-                pass
-
-            lmap(action, inc.poll(inc.queue(settings.ARTICLE_EVENT_QUEUE)))
-
-        except ValueError as err:
-            LOG.error(err)
+        if not settings.EVENT_QUEUE:
+            LOG.error("no queue name found. a queue name can be set in your 'app.cfg'. see example file 'elife.cfg'")
             sys.exit(1)
 
-        except:
-            LOG.exception("unhandled exception attempting to ingest article")
-            raise
+        LOG.info("attempting connection %s ...", settings.EVENT_QUEUE)
+
+        def action(event):
+            try:
+                LOG.debug("handling event %s" % event)
+                event = json.loads(event)
+                objid = event['id']
+                handlers = {
+                    'article': download_regenerate_article,
+                    'presspackage': download_regenerate_presspackage,
+                }
+                handlers[event['type']](objid)
+
+            except BaseException as err:
+                LOG.exception("unhandled exception attempting to handle event %s", event)
+
+            return None # important, ensures results don't accumulate
+
+        try:
+            import newrelic.agent
+            action = newrelic.agent.background_task()(action)
+        except ImportError:
+            pass
+
+        # `any` doesn't accumulate a list of results in memory so long as
+        # `action` returns false-y values.
+        any(action(event) for event in inc.poll(inc.queue(settings.EVENT_QUEUE)))
 
         sys.exit(0)
