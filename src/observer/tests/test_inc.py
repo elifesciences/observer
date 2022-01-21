2d1bc4770256d82f6461c6bf708fd168f70997e1
Luke <lsh-0@users.noreply.github.com>
2022-01-21 17:32:18 +1030

    review feedback

--- src/observer/tests/test_inc.py
+++ src/observer/tests/test_inc.py
@@ -1,7 +1,9 @@
 import json
 from . import base
 from unittest.mock import patch, Mock
-from observer import inc, models, ingest_logic
+from observer import inc, models, ingest_logic, consume
+import pytest
+import requests
 
 class One(base.BaseCase):
 
@@ -47,3 +49,30 @@ def test_handling_unexpected_error():
         with patch('observer.ingest_logic.download_regenerate', side_effect=RuntimeError('no pants')):
             inc.handler(dummy_event)
             assert mock.exception.called
+
+@pytest.mark.django_db
+def test_404_deletes_item():
+    "receiving a 404 for content that exists causes the item to be deleted"
+    item_id = 'ecc32978'
+    fixture = base.jsonfix('interviews/ecc32978.json')
+
+    consume.upsert(item_id, models.INTERVIEW, fixture)
+    ingest_logic._regenerate_item(models.INTERVIEW, item_id)
+
+    assert models.Content.objects.count() == 1
+    assert models.RawJSON.objects.count() == 1
+
+    # simulate receiving an event
+    dummy_event = Mock()
+    dummy_event.body = json.dumps({'type': models.INTERVIEW, 'id': item_id})
+
+    # simulate a 404 error response
+    response = requests.Response()
+    response.status_code = 404
+    exc = requests.exceptions.RequestException()
+    exc.response = response
+    with patch('observer.ingest_logic.download_item', side_effect=exc): # prevent downloading other versions
+        inc.handler(dummy_event)
+
+    assert models.Content.objects.count() == 0
+    assert models.RawJSON.objects.count() == 0
