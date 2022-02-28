c1348bb052b797cc9a7d1bb74a49da95b1696da8
Luke <lsh-0@users.noreply.github.com>
2022-02-28 16:50:36 +1030

    daily.sh, disables elife-metrics scraping.

--- daily.sh
+++ daily.sh
@@ -4,4 +4,5 @@
 # other content types (articles, presspackages, digests, etc) are updated via the message bus.
 # `load_from_api` is able to do adhoc imports of any content though.
 
-./manage.sh load_from_api --target profiles elife-metrics community
+#./manage.sh load_from_api --target profiles elife-metrics community
+./manage.sh load_from_api --target profiles community
