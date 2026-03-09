2c4502736678a9551874e82672864d75a95e844b
LinaKind <l.kinduryte@elifesciences.org>
2026-03-09 13:51:56 +0200

    Fix hostname for ci
    https://github.com/elifesciences/issues/issues/9497

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -2,6 +2,10 @@
 set -ex
 
 for path in / /report/latest-articles /report/upcoming-articles; do
-    [ $(curl --write-out %{http_code} --silent --output /dev/null https://$(hostname)$path) == 200 ]
+    hostname=$(hostname)
+    if [[ "$hostname" == "ci--observer.elifesciences.org" ]]; then
+        hostname="ci-observer.elifesciences.org"
+    fi
+    [ $(curl --write-out %{http_code} --silent --output /dev/null https://$hostname$path) == 200 ]
 done
 
